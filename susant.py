99a0f623b3edc553734e9f0daf2798bcce5a3bfa
Susant Puri <susantpuri@Susants-MBP.lan>
2025-12-25 17:24:48 -0500

    terraform

--- susant.py
+++ susant.py
@@ -116,8 +116,44 @@ elif year % 4 == 0:
 else:
     print('not leap year')
 
+#### while loop #######
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess:'))
+    guess_count += 1
+    if guess == secret_number:
+        print('you won!')
+        break
+else:
+    print('sorry, you failed')
 
-s1 = 'software'
-s2 = 'Hardware'
+    
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command == "start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is stopped")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "help":
+        print(""""
+    start - to start the car
+    stop - to stop the car
+    quit - to quit
+""")
+    elif command =="quit":
+        break
+    else:
+        print("sorry, I don't understand")
 
-s1 > s2
