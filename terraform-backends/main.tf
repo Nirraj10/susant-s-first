08504cff4a027e083a321d555f55985774cc6e52
Susant Puri <susantpuri@Susants-MBP.lan>
2025-12-29 11:02:46 -0500

    hello

--- terraform-backends/main.tf
+++ terraform-backends/main.tf
@@ -1,23 +1,26 @@
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = "~.1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
+        source = "hasicorp/aws"
+        version = "~> 5.0"
     }
-    random = {
-      source = "hasicorp/random"
-      version = " ~> 3.0"
-    }
-  }
-backend "s3" {
-  bucket = "name-of-bucket"
-  key = "state.tfstate"
-  region = "us-east-1"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  }
+}
 
+provider "aws" {
+  region = "eu-west-1"
+  alias = eu-west
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+    bucket = "my-first-989"
+}
+resource "aws_s3_bucket" "bucket_eu_west_1" {
+    bucket = "my-second-333"
+    provider = aws.eu-west
+}
