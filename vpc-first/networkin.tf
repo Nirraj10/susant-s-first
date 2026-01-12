fa23f23284596062eebacb3f066272fc839d969a
Susant Puri <susantpuri@Susants-MBP.lan>
2026-01-12 11:10:38 -0500

    hi

--- vpc-first/networkin.tf
+++ vpc-first/networkin.tf
@@ -1,32 +1,32 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "susant-vpc"
+locals {
+  common_tags = {
     ManagedBy = "Terraform"
     Project = "network_vpc"
   }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = merge(local.common_tags, {
+    Name = "susant-vpc"
+  })
 }
 
 resource "aws_subnet" "public" {
   vpc_id = aws.vpc.main.vpc_id
   cidr_block = "10.0.0.0/24"
 
-    tags = {
-    Name = "susant-vpc"
-    ManagedBy = "Terraform"
-    Project = "network_vpc"
-  }
+  tags = merge(local.common_tags, {
+    Name = "susant-subnet"
+  })
 }
-
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-  Name = "my-frist-igw"
-  ManagedBy = "Terraform"
-  Project = "network_vpc"
-  }
+  tags = merge(local.common_tags, {
+    Name = "my-first-igw"
+  })
 }
 
 resource "aws_route_table" "public" {
